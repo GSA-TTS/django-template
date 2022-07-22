41b51c3e264025e1c9edc386db8fd57884e252df
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2022-07-22 11:15:08 -0500

    Fix black formatting

--- django_template/template_command.py
+++ django_template/template_command.py
@@ -4,23 +4,31 @@ import click
 
 from .project_creator import ProjectCreator
 
+
 @click.command()
-@click.option("--app-name", default=None, type=str, show_default=False,
-              prompt="What is the name of your application",
-              help="Name of the new application",
-             )
-@click.option("--uswds/--no-uswds",
-              prompt="Would you like to install USWDS (requires node to already be installed)",
-              help="Use USWDS in the new application",
-             )
-@click.option("--circleci/--no-circleci",
-              prompt="Would you like to set up CircleCI for CI/CD",
-              help="Configure CircleCI for the new application",
-             )
-@click.option("--github-actions/--no-github-actions",
-              prompt="Would you like to set up Github Actions for CI/CD",
-              help="Configure Github Actions for the new application",
-             )
+@click.option(
+    "--app-name",
+    default=None,
+    type=str,
+    show_default=False,
+    prompt="What is the name of your application",
+    help="Name of the new application",
+)
+@click.option(
+    "--uswds/--no-uswds",
+    prompt="Would you like to install USWDS (requires node to already be installed)",
+    help="Use USWDS in the new application",
+)
+@click.option(
+    "--circleci/--no-circleci",
+    prompt="Would you like to set up CircleCI for CI/CD",
+    help="Configure CircleCI for the new application",
+)
+@click.option(
+    "--github-actions/--no-github-actions",
+    prompt="Would you like to set up Github Actions for CI/CD",
+    help="Configure Github Actions for the new application",
+)
 def template_command(app_name, uswds, circleci, github_actions):
     """Run the command line script."""
     config = {
