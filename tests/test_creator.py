0c051c3a338a16c3c39d55bc4782aba407767359
JeanMarie Mariadassou <JeanMarie.Mariadassou@gsa.gov>
2022-07-25 13:37:18 -0700

    format with black

--- tests/test_creator.py
+++ tests/test_creator.py
@@ -4,6 +4,7 @@ import os
 from django_template.project_creator import ProjectCreator
 from pathlib import Path
 
+
 def test_yes(monkeypatch, tmp_path):
     creator = ProjectCreator(tmp_path, config={})
     with monkeypatch.context() as m:
@@ -27,12 +28,15 @@ def test_run(monkeypatch, tmp_path):
     creator = ProjectCreator(tmp_path, config={})
     dest_dir = creator.run()
     assert os.path.isdir(dest_dir)
-    assert len(sorted(Path(dest_dir).glob('**/manage.py'))) == 1
-    assert len(sorted(Path(dest_dir).glob('**/prod.py'))) == 1
+    assert len(sorted(Path(dest_dir).glob("**/manage.py"))) == 1
+    assert len(sorted(Path(dest_dir).glob("**/prod.py"))) == 1
+
 
 def test_run_uswds(monkeypatch, tmp_path):
     creator = ProjectCreator(tmp_path, config={})
-    creator.config['uswds'] = 'y'
+    creator.config["uswds"] = "y"
     dest_dir = creator.run()
     assert os.path.isdir(dest_dir)
-    assert len(sorted(Path(dest_dir).glob('**/gulpfile.js'))) == 3 # 3 copies get created due to npm commands
+    assert (
+        len(sorted(Path(dest_dir).glob("**/gulpfile.js"))) == 3
+    )  # 3 copies get created due to npm commands
